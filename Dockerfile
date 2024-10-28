1d3e345c3a5e07de56c9a992d4b989b45281b7d7
Andrea Liz <andrealiznm@gmail.com>
2024-10-27 22:30:55 -0500

    add calculadora.py Dockerfile con vaiables de entorno

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,13 @@ WORKDIR /app
 COPY calculadora.py /app/
 COPY requirements.txt /app/
 
+# Pasando las variables de entorno para calcualdora.py
+ENV OPERACION=1
+ENV NUM1=10
+ENV NUM2=5
+
 # Instalando las dependencias
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Ejecucion de la aplicación
-CMD ["python3", "calculadora.py"]
+CMD ["python", "calculadora.py"]
--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,6 @@
-# Calculadora simple en Python
+import os
 
+# Calculadora simple en Python
 def sumar(a, b):
     return a + b
 
@@ -15,32 +16,24 @@ def dividir(a, b):
     else:
         return "Error: División por cero"
 
-def main():
-    print("Selecciona la operación:")
-    print("1. Sumar")
-    print("2. Restar")
-    print("3. Multiplicar")
-    print("4. Dividir")
+def calculadora():
+    # Obtener la opción de operación y los números de las variables de entorno
+    opcion = os.getenv("OPERACION", "1")  # Valor por defecto: "1" (Sumar)
+    num1 = float(os.getenv("NUM1", 0)) # Si NUM1 no está definido, usará 0 como valor por defecto
+    num2 = float(os.getenv("NUM2", 0))
 
-    opcion = input("Introduce la opción (1/2/3/4): ")
-
-    if opcion in ['1', '2', '3', '4']:
-        num1 = float(input("Introduce el primer número: "))
-        num2 = float(input("Introduce el segundo número: "))
-
-        if opcion == '1':
-            print(f"{num1} + {num2} = {sumar(num1, num2)}")
-        elif opcion == '2':
-            print(f"{num1} - {num2} = {restar(num1, num2)}")
-        elif opcion == '3':
-            print(f"{num1} * {num2} = {multiplicar(num1, num2)}")
-        elif opcion == '4':
-            resultado = dividir(num1, num2)
-            print(f"{num1} / {num2} = {resultado}")
+    # Ejecutar la operación seleccionada
+    if opcion == '1':
+        print(f"{num1} + {num2} = {sumar(num1, num2)}")
+    elif opcion == '2':
+        print(f"{num1} - {num2} = {restar(num1, num2)}")
+    elif opcion == '3':
+        print(f"{num1} * {num2} = {multiplicar(num1, num2)}")
+    elif opcion == '4':
+        resultado = dividir(num1, num2)
+        print(f"{num1} / {num2} = {resultado}")
     else:
         print("Opción no válida")
 
 # Ejecutar la calculadora
-#calculadora()
-if __name__ == "__main__":
-    main()
+calculadora()
